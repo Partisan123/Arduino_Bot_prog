51a766701541de0e2cb6893b190157be9c45bf2d
Partisan123 <90345743+Partisan123@users.noreply.github.com>
2023-10-18 10:21:20 +0300

    Add files via upload
    updated code

---
+++ main/drive.h
@@ -0,0 +1,86 @@
+//#include <Arduino.h>
+
+
+/*
+ * Motor handler 
+ * 
+ * This class handles Motor instance creation. 
+ * Motor control, power and direction. 
+ * Useful for L298 and similar motor controllers.   
+ * 
+ */
+
+
+class Drive {
+
+    public:
+
+       // bool flipDir;
+        int pwm_power;
+        int motor;
+        int dir1;
+        int dir2;
+
+        bool flipDir;
+        
+        Drive(int motorPin, int dirPin1, int dirPin2, bool flipDir){
+            this->motor = motorPin;
+            this->dir1 = dirPin1;
+            this->dir2 = dirPin2;
+            this->flipDir = flipDir;
+            init();
+
+        }
+        Drive(){
+            this->motor = 0;
+            this->dir1 = 0;
+            this->dir2 = 0;
+            
+            init();
+
+        }
+
+
+        void init(){
+        
+            pinMode(motor, OUTPUT);
+            pinMode(dir1, OUTPUT);
+            pinMode(dir2, OUTPUT);
+                
+        }
+
+        void driveMotForward(int pwr){  
+            
+            if(flipDir == true){           
+                digitalWrite(dir1, HIGH);
+                digitalWrite(dir2, LOW);
+            }
+            else{    
+                digitalWrite(dir1, LOW);
+                digitalWrite(dir2, HIGH);
+            }
+
+            analogWrite(motor, pwr);
+
+  
+        }
+
+        void driveMotBackward(int pwr){  
+            
+            if(flipDir == true){           
+                digitalWrite(dir1, LOW);
+                digitalWrite(dir2, HIGH);
+            }
+            else{    
+                digitalWrite(dir1, HIGH);
+                digitalWrite(dir2, LOW);
+            }
+
+            analogWrite(motor, pwr);
+
+  
+        }
+
+
+
+};
---
+++ main/pinout.h
@@ -0,0 +1,30 @@
+
+// pins for rotary Hall encoder inputs
+#define LF_ENCODER_P 10  //Encoder pin 1
+#define LF_ENCODER_N 11   //Encoder pin 2
+
+#define LB_ENCODER_P 14 
+#define LB_ENCODER_N 25
+
+#define RF_ENCODER_P 27
+#define RF_ENCODER_N 26
+
+#define RB_ENCODER_P 17 
+#define RB_ENCODER_N 16
+
+// pins for the motor outputs
+#define motor_lf 0    //Motor PWM signal 
+#define dir_lf1 6      //Direction pin 1
+#define dir_lf2 7      //Direction pin 2
+
+#define motor_lb 1
+#define dir_lb1 8
+#define dir_lb2 9
+
+#define motor_rf 28
+#define dir_rf1  21
+#define dir_rf2  20
+
+#define motor_rb  22
+#define dir_rb1  19
+#define dir_rb2  18
